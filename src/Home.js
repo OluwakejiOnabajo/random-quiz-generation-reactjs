b384260acd7f5cc6d6e67a2606f4a646eba35912
Oluwakeji <onabajooluwakeji.daniel@gmail.com>
2022-08-18 16:39:58 +0100

    update

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,13 @@
 import InfoPage from "./components/infoPage/InfoPage";
 import Rules from "./components/rules/Rules";
+import Question from "./components/question/Question";
 
 const Home = () => {
   return (
     <>
     {/* <InfoPage /> */}
-    <Rules />
+    {/* <Rules /> */}
+    <Question />
     </>
   )
 }
--- src/components/question/Question.js
+++ src/components/question/Question.js
@@ -1,9 +1,34 @@
-import './Questions.css';
+import './Question.css';
 import Card from '../card/Card';
 
 const Question = () => {
   return (
-    <div>Question</div>
+    <Card>
+  <div className='question-header'>
+    <div className="title">Awesome Quiz Application</div>
+    <div className="timer">
+      <div className="time_left_txt">Time Left</div>
+      <div className="timer_sec">15</div>
+    </div>
+    <div className="time_line" />
+  </div>
+  <div className='question'>
+    <div className="que_text">
+      {/* Here I've inserted question from JavaScript */}
+    </div>
+    <div className="option_list">
+      {/* Here I've inserted options from JavaScript */}
+    </div>
+  </div>
+  {/* footer of Quiz Box */}
+  <div className='question-footer'>
+    <div className="total_que">
+      {/* Here I've inserted Question Count Number from JavaScript */}
+    </div>
+    <button className="next_btn">Next Que</button>
+  </div>
+
+    </Card>
   )
 }
 
